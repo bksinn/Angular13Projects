d74448945775e1dd40380badd08a8d5afff98706
Bharat Singh <bharat@pingyo.com>
2021-12-24 14:37:25 -0500

    Add Clarity data table

---
+++ issue-tracker/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClarityModule } from '@clr/angular';
+
+import { AppComponent } from './app.component';
+import { IssueListComponent } from './issue-list/issue-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IssueListComponent
+  ],
+  imports: [
+    BrowserModule,
+    ClarityModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+})
+export class AppModule { }
