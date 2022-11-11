45fcd1d85fad98764bf306e423ed73782ddb17ae
Bharat Singh <bharat@pingyo.com>
2022-11-10 19:38:39 -0600

    Configure service, add POI

--- packt/apps/tour/src/app/app.module.ts
+++ packt/apps/tour/src/app/app.module.ts
@@ -4,17 +4,48 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { RouterModule } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
   declarations: [AppComponent, AdminComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, RouterModule.forRoot([
-    {path: 'admin', loadChildren: () => import ('@packt/admin').then(m => m.AdminModule)},
-    {path: 'tour', loadChildren: () => import ('@packt/visitor').then(m => m.VisitorModule)},
-    {path: '', pathMatch: 'full', redirectTo: 'tour'}
-  ])],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: 'admin',
+        loadChildren: () => import('@packt/admin').then((m) => m.AdminModule),
+      },
+      {
+        path: 'tour',
+        loadChildren: () =>
+          import('@packt/visitor').then((m) => m.VisitorModule),
+      },
+      { path: '', pathMatch: 'full', redirectTo: 'tour' },
+    ]),
+    StoreModule.forRoot(
+      {},
+      {
+        metaReducers: !environment.production ? [] : [],
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictStateImmutability: true,
+        },
+      }
+    ),
+    EffectsModule.forRoot([]),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    StoreRouterConnectingModule.forRoot(),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
