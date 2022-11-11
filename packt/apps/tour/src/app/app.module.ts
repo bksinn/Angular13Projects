8a3619839886f80e08f6f327c3287bf80498800f
Bharat Singh <bharat@pingyo.com>
2022-11-10 19:20:00 -0600

    Scaffold projects with nx

--- packt/apps/tour/src/app/app.module.ts
+++ packt/apps/tour/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { RouterModule } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, AdminComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, RouterModule.forRoot([
+    {path: 'admin', loadChildren: () => import ('@packt/admin').then(m => m.AdminModule)},
+    {path: 'tour', loadChildren: () => import ('@packt/visitor').then(m => m.VisitorModule)},
+    {path: '', pathMatch: 'full', redirectTo: 'tour'}
+  ])],
   providers: [],
   bootstrap: [AppComponent],
 })
